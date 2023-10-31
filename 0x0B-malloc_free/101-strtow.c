486248e1826cad99e8a03b772dfd2662ee8873d5
1Light <redclover1.618@gmail.com>
2023-10-31 01:20:08 -0700

    updated answer

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,116 +1,91 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * is_separator - check if a character is a separator
- * @c: character to check
- * @separators: string containing separator characters
- * Return: 1 if c is a separator, 0 otherwise
+ * count_word - Helper function to count the number of words in a string
+ * @s: The string to evaluate
+ *
+ * Return: The number of words
  */
-int is_separator(char c, char *separators)
+int count_word(char *s)
 {
-    while (*separators)
-    {
-        if (c == *separators)
-            return 1;
-        separators++;
-    }
-    return 0;
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
 }
 
 /**
- * strtow - splits a string into words
- * @str: string to split
- * Return: pointer to an array of strings (Success) or NULL (Error)
+ * split_word - Helper function to split a string into words
+ * @str: The string to split
+ * @matrix: Array of strings to store the result
+ */
+void split_word(char *str, char **matrix)
+{
+	char *tmp;
+	int i, k = 0, c, start = 0, end;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *)malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return;
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	matrix[k] = NULL;
+}
+
+/**
+ * strtow - Splits a string into words
+ * @str: The string to split
+ *
+ * Return: Pointer to an array of strings (Success) or NULL (Error)
  */
 char **strtow(char *str)
 {
-    if (str == NULL || *str == '\0')
-        return NULL;
+	char **matrix;
+	int words;
 
-    int words = 0;
-    int in_word = 0;  // Flag to track if we are inside a word
-    char **matrix;
-    char *current_word = NULL;
+	if (str == NULL || *str == '\0')
+		return (NULL);
 
-    // First pass: Count the number of words
-    for (int i = 0; str[i]; i++)
-    {
-        if (!is_separator(str[i], " \t\n"))
-        {
-            if (!in_word)
-            {
-                in_word = 1;
-                words++;
-            }
-        }
-        else
-        {
-            in_word = 0;
-        }
-    }
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
 
-    // Allocate memory for the matrix
-    matrix = malloc((words + 1) * sizeof(char *));
-    if (matrix == NULL)
-        return NULL;
+	matrix = (char **)malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
 
-    // Second pass: Split the string into words
-    in_word = 0;
-    words = 0;
-    for (int i = 0; str[i]; i++)
-    {
-        if (!is_separator(str[i], " \t\n"))
-        {
-            if (!in_word)
-            {
-                in_word = 1;
-                current_word = str + i;
-            }
-        }
-        else
-        {
-            if (in_word)
-            {
-                in_word = 0;
-                int length = (str + i) - current_word;
-                matrix[words] = malloc(length + 1);
-                if (matrix[words] == NULL)
-                {
-                    // Clean up on allocation failure
-                    for (int j = 0; j < words; j++)
-                        free(matrix[j]);
-                    free(matrix);
-                    return NULL;
-                }
-                for (int j = 0; j < length; j++)
-                    matrix[words][j] = current_word[j];
-                matrix[words][length] = '\0';
-                words++;
-            }
-        }
-    }
+	split_word(str, matrix);
 
-    matrix[words] = NULL;
-    return matrix;
-}
-
-/**
- * main - Test the strtow function
- * Return: Always 0
- */
-int main(void)
-{
-    char *str = "Talk is cheap. Show me the code.";
-    char **words = strtow(str);
-    if (words)
-    {
-        for (int i = 0; words[i]; i++)
-        {
-            printf("[%s]\n", words[i]);
-            free(words[i]);
-        }
-        free(words);
-    }
-    return (0);
+	return (matrix);
 }
